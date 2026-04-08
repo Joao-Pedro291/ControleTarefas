40f0c6157fc339457ab4765b93b7c9b9748a3b9e
Joao Pedro <joaosao291@gmail.com>
2026-04-08 17:25:04 -0300

    inicialização do projeto

---
+++ ControleTarefas/Tarefa.cs
@@ -0,0 +1,15 @@
+﻿namespace ControleTarefas;
+
+internal class Tarefa
+{
+    public Tarefa(string titulo, bool concluida)
+    {
+        Titulo = titulo;
+        Concluida = false;
+    }
+
+    public string? Titulo { get; set; }
+    public bool Concluida { get; set; }
+
+}
+    
